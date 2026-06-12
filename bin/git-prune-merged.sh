11c9aacf23bede7931f3e17a4b8bb29dfbb929d8
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2026-06-13 01:24:04 +0200

    git-prune-merged: enable strict mode and tolerate no branches to prune

--- bin/git-prune-merged.sh
+++ bin/git-prune-merged.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 main_branch=$(git remote show origin | awk '/HEAD branch/ {print $NF}')
 main_branch_escaped=$(printf '%s' "$main_branch" | sed 's/[.$+(){}|^*?]/\\&/g')
-git fetch && git branch --merged "origin/$main_branch" | grep -vE "^\s*(\*|\+|($main_branch_escaped$))" | xargs -n 1 git branch -d
+git fetch && git branch --merged "origin/$main_branch" | grep -vE "^\s*(\*|\+|($main_branch_escaped$))" | xargs -n 1 git branch -d || true
