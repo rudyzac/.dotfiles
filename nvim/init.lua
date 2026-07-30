9e751c677db5dcaa529d84500a274e16ec662caf
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2026-07-31 01:25:59 +0200

    Neovim: disable flash.nvim backdrop dimming

--- nvim/init.lua
+++ nvim/init.lua
@@ -128,7 +128,11 @@ require("lazy").setup({
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      highlight = {
+        backdrop = false, -- don't dim the rest of the buffer while jumping
+      },
+    },
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
